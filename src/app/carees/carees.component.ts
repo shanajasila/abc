71c6541d2b3526459d6893078921535e7ef393b3
shanajasila <shanajasilam@gmail.com>
2022-12-05 07:47:49 +0530

    career

---
+++ src/app/carees/carees.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carees',
+  templateUrl: './carees.component.html',
+  styleUrls: ['./carees.component.css']
+})
+export class CareesComponent {
+
+}
